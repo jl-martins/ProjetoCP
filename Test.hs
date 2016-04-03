a40181d2a90b8283030fa9d365c624cf981f9d08
João Pereira <joaopereira_19@hotmail.com>
2016-04-03 20:45:11 +0100

    Juntei os testes num so ficheiro

--- Test.hs
+++ Test.hs
@@ -39,19 +39,56 @@ g1 = Graph {nodes = fromList [1],
             edges = fromList [Edge 1 1]
            }
 
+g2 :: Graph Int
+g2 = Graph {nodes = fromList [1,2], 
+            edges = fromList [Edge 1 1, Edge 1 2]
+           }
+
+{- Ao acrescentarmos a aresta (5,1) às arestas de 'forest1', o vértice 5 passa a ter dois
+   vértices adjacentes (os vértices 1 e 6), pelo que o grafo resultante não é uma floresta -}
+g3 :: Graph Int
+g3 = Graph {nodes = nodes forest1,
+            edges = edges forest1 `Set.union` (fromList [Edge 5 1])
+           }
+
+g4 :: Graph Int
+g4 = Graph {nodes = nodes g2,
+            edges = fromList [Edge 2 1]
+           }
+
+union_g4_g1 :: Graph Int
+union_g4_g1 = Graph {nodes = fromList [1,2],
+                     edges = fromList [Edge 1 1, Edge 2 1]
+                    }
+
+forest1 :: Graph Int
+forest1 = Graph {nodes = fromList [1,2,3,4,5,6],
+                edges = fromList [Edge 1 3, Edge 3 2, Edge 4 3, Edge 5 6]
+               }
+
+
+nodesGLarge :: Set Int
+nodesGLarge = fromList [1..15]
+
+edgesGLarge :: Set (Edge Int)
+edgesGLarge = fromList [Edge 1 1, Edge 1 3, Edge 2 1, Edge 3 2, Edge 3 5, Edge 4 1, Edge 4 2, 
+                        Edge 4 12, Edge 4 13, Edge 5 6, Edge 5 8, Edge 6 7, Edge 6 8, Edge 6 10, 
+                        Edge 7 10, Edge 8 9, Edge 8 10, Edge 9 11, Edge 10 9, Edge 10 11, Edge 10 14, 
+                        Edge 11 12, Edge 11 14, Edge 12 13, Edge 13 11, Edge 13 15, Edge 14 13, Edge 15 14]
+
+-- http://www.greatandlittle.com/studios/public/blowup-images/Dart/.directed_graph_m.jpg
+gLarge :: Graph Int
+gLarge = Graph {nodes = nodesGLarge, edges = edgesGLarge}
+
 gInvalid :: Graph Int
 gInvalid = Graph {nodes = fromList [1,2],
-                  edges = fromList [(1,3)]
+                  edges = fromList [Edge 1 3]
                  }
 
 gDag :: Graph Int
 gDag = Graph {nodes = fromList [1,2,3],
-              edges = fromList [(1,2),(1,3)]
+              edges = fromList [Edge 1 2, Edge 1 3]
              }
-
--- Um exemplo de um teste unitário.
-test_adj :: Test
-test_adj = adj g1 1 ~?= fromList [Edge 1 1]
 
 --
 -- Tarefa 1
@@ -61,7 +98,9 @@ test_adj = adj g1 1 ~?= fromList [Edge 1 1]
 --
 
 tests :: Test
-tests = TestList [test_swap, test_isDag, test_adj, test_isPathOf]
+tests = TestList [test_swap, test_isDAG, test_adj, test_isPathOf, 
+                  test_isEmpty, test_isForest, test_isSugraphOf, 
+                  test_union, test_reachable, test_path]
 
 test_swap :: Test
 test_swap = swap (Edge 1 2) ~?= (Edge 2 1)
@@ -69,13 +108,13 @@ test_swap = swap (Edge 1 2) ~?= (Edge 2 1)
 test_isValid :: Test
 test_isValid = isValid gInvalid ~?= False
 
-test_isDag :: Test
-test_isDag = TestList [isDag gInvalid ~?= False,
-                       isDag g1 ~?= False,
-                       isDag gDag ~?= True]
+test_isDAG :: Test
+test_isDAG = TestList [isDAG gInvalid ~?= False,
+                       isDAG g1 ~?= False,
+                       isDAG gDag ~?= True]
 
 test_transpose :: Test 
-test_transpose = transpose gDag ~?= Graph {nodes = fromList [1..3], edges = fromList[(2,1),(3,1)]}
+test_transpose = transpose gDag ~?= Graph {nodes = fromList [1..3], edges = fromList [Edge 2 1, Edge 3 1]}
 
 test_adj :: Test
 test_adj = TestList [adj gLarge 4 ~?= Set.fromList [Edge 4 1, Edge 4 2, Edge 4 12, Edge 4 13],
@@ -89,18 +128,58 @@ test_isPathOf = TestList [isPathOf [] g1 ~?= True,
                           isPathOf [Edge 1 3, Edge 3 5, Edge 5 6, Edge 6 7, Edge 7 10, Edge 10 9, Edge 9 11, 
                                     Edge 11 14, Edge 14 13, Edge 13 15] gLarge ~?= True,
                           --falha 1a condiçao
-                          isPathOf [Edge 1 12, Edge 12 13] ~?= False,
+                          isPathOf [Edge 1 12, Edge 12 13] gLarge ~?= False,
                           -- falha 2a
-                          isPathOf [Edge 1 3, Edge 5 6] ~?= False,
+                          isPathOf [Edge 1 3, Edge 5 6] gLarge ~?= False,
                           -- falha na 3a
                           isPathOf [Edge 1 3, Edge 3 5, Edge 5 9] gLarge ~?= False]
 
 -- falta completar o caso nao trivial
 test_topo :: Test
-test_topo = TestList [topo empty ~?= []]
+test_topo = TestList [{-topo Graph.empty ~?= []-}]
+
+-- JM
+
+test_isEmpty :: Test
+test_isEmpty = TestList [TestCase $ assertBool "" (isEmpty Graph.empty), 
+                         isEmpty g1 ~?= False
+                        ]
+
+
+test_isForest :: Test
+test_isForest = TestList [TestCase $ assertBool "" (isForest forest1),
+                          isForest g1 ~?= False, -- não é um DAG
+                          isForest g3 ~?= False -- o vértice 5 tem 2 vértices adjacentes
+                         ] 
+
+test_isSugraphOf :: Test
+test_isSugraphOf = TestList [isSubgraphOf g1 g2 ~?= True,
+                             isSubgraphOf (Graph.empty) g1 ~?= True, -- trivialmente válido
+                             isSubgraphOf g2 g1 ~?= False,
+                             isSubgraphOf g4 g2 ~?= False
+                            ]
+
+test_union :: Test
+test_union = TestList [g1 `Graph.union` g1 ~?= g1,
+                       Graph.empty `Graph.union` g1 ~?= g1,
+                       g4 `Graph.union` g1 ~?= union_g4_g1
+                      ]
+
+test_reachable :: Test
+test_reachable = TestList [reachable g4 1 ~?= fromList [1],
+                           reachable forest1 1 ~?= fromList [1, 2, 3]
+                          ]
+
+test_path :: Test
+test_path = TestList [path gLarge 4 7 ~?= pathGLarge4_7,
+                      path gLarge 1 4 ~?= Nothing]
+    where pathGLarge4_7 :: Maybe (Graph.Path Int) -- caminho mais curto de 4 para 7 em gLarge
+          pathGLarge4_7 = Just [Edge 4 1, Edge 1 3, Edge 3 5, Edge 5 6, Edge 6 7]
 
 -- bft, topo
       
+
+
 main = runTestTT tests
 
 --
